869a31b5de5a2749be6af3dd5c4d11d0d65b5d32
tomhop321 <85928808+tomhop321@users.noreply.github.com>
2023-07-30 14:00:29 +0200

    biblotrki23231321
    ADADSQADCWADCWA

---
+++ node/serwe.js
@@ -0,0 +1,21 @@
+const exporess=require('express');
+
+const port = 3000
+
+const app = exporess()
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render("index",{
+    pa:'helllooo',
+    ge:'nooooooodee'
+  }
+
+)
+} )
+app.get('/xd', (req, res) => {
+    res.send("hello nodedddddddd")
+    console.log(4444)
+
+} )
+app.listen(port)
---
+++ node/xd.js
@@ -0,0 +1,13 @@
+var t = 5
+var g = 2
+
+const xd = () => {
+  console.log("ksjdfkjskfj")
+}
+console.log("fff")
+
+const xdd = (a, b) => {
+  console.log(a + b)
+}
+
+module.export = {xd,xdd}
